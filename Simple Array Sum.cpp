6d3a04b87834815feaecfa3dc8bbd3d9b02db02c
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2019-06-24 10:57:34 +0600

    Add files via upload

---
+++ Simple Array Sum.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define           ll               long long int
+#define           pb               push_back
+#define           pii              pair<ll,ll>
+#define           mx               100005
+int main()
+{
+    ll a,i,x,sum = 0;
+    cin>>a;
+    while(a--)
+    {
+        cin>>i;
+        sum+=i;
+    }
+    cout<<sum;
+}
